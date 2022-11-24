8c42e420fc32df10bd9749dcefbf173a553f5b4f
Rafaelly <rafaellylemos@hotmail.com>
2022-11-24 12:03:26 -0300

    att alphabetical order

--- script.js
+++ script.js
@@ -1,77 +1,77 @@
 function CarregaDicionario() {
     var biografias = {
         bio01: {
-            nome:"Marielle Franco",
-            imagem: "https://i.postimg.cc/8fS8KSGX/marielle-franco.png",
-            descricao: "Marielle Francisco da Silva, conhecida como Marielle Franco, foi uma socióloga, feminista, militante dos direitos humanos e política brasileira. Filiada ao Partido Socialismo e Liberdade (PSOL), elegeu-se vereadora do Rio de Janeiro para a Legislatura 2017-2020, durante a eleição municipal de 2016, com a quinta maior votação. Crítica da intervenção federal no Rio de Janeiro e da Polícia Militar, denunciava constantemente abusos de autoridade por parte de policiais contra moradores de comunidades carentes. Em 14 de março de 2018, foi assassinada a tiros",
-            citacao: "Quantos mais vão precisar morrer para que essa guerra aos pobres acabe?",
+        nome: "Angela Davis",     
+        imagem: "https://i.postimg.cc/LJ6SF21r/angela-davis.png",     
+        descricao: "Angela Yvonne Davis é uma professora e filósofa socialista estado-unidense que alcançou notoriedade mundial na década de 1970 como integrante do Partido Comunista dos Estados Unidos, dos Panteras Negras, por sua militância pelos direitos das mulheres e contra a discriminação social e racial nos Estados Unidos e por ser personagem de um dos mais polêmicos e famosos julgamentos criminais da recente história dos Estados Unidos.",
+        citacao: "Não aceito mais as coisas que não posso mudar, estou mudando as coisas que não posso aceitar.",
         },
         bio02: {
-            nome: "Lélia Gonzalez",
-            imagem: "https://i.postimg.cc/2q1f3GTG/lelia-gonzalez.png",
-            descricao: "Lélia Gonzalez foi uma intelectual, política, professora e antropóloga brasileira. Ajudou a fundar instituições como o Movimento Negro Unificado (MNU), o Instituto de Pesquisas das Culturas Negras (IPCN), o Coletivo de Mulheres Negras N'Zinga e o Olodum. Sua militância em defesa da mulher negra levou-a ao Conselho Nacional dos Direitos da Mulher (CNDM), no qual atuou de 1985 a 1989. Foi candidata a deputada federal pelo PT, elegendo-se primeira suplente. Nas eleições seguintes, em 1986, candidatou-se a deputada estadual pelo PDT, novamente elegendo-se suplente.",
-            citacao:
-            "A gente não nasce negro, a gente se torna negro. É uma conquista dura, cruel e que se desenvolve pela vida da gente afora.",
-          },     
-          bio03: {      
-            nome: "Angela Davis",     
-            imagem: "https://i.postimg.cc/LJ6SF21r/angela-davis.png",     
-            descricao: "Angela Yvonne Davis é uma professora e filósofa socialista estado-unidense que alcançou notoriedade mundial na década de 1970 como integrante do Partido Comunista dos Estados Unidos, dos Panteras Negras, por sua militância pelos direitos das mulheres e contra a discriminação social e racial nos Estados Unidos e por ser personagem de um dos mais polêmicos e famosos julgamentos criminais da recente história dos Estados Unidos.",
-            citacao: "Não aceito mais as coisas que não posso mudar, estou mudando as coisas que não posso aceitar.",
-          },
-          bio04: {
-            nome: "Conceição Evaristo",
-            imagem: "https://i.postimg.cc/Z9XzNNMS/conceicao-evaristo.png",
-            descricao:  "Maria da Conceição Evaristo de Brito nasceu em Belo Horizonte, em 1946. De origem humilde, migrou para o Rio de Janeiro na década de 1970. Graduada em Letras pela UFRJ, trabalhou como professora da rede pública de ensino da capital fluminense. É Mestre em Literatura Brasileira pela PUC do Rio de Janeiro, com a dissertação Literatura Negra: uma poética de nossa afro-brasilidade (1996), e Doutora em Literatura Comparada na Universidade Federal Fluminense, com a tese Poemas malungos, cânticos irmãos (2011), na qual estuda as obras poéticas dos afro-brasileiros Nei Lopes e Edimilson de Almeida Pereira em confronto com a do angolano Agostinho Neto.",
-            citacao: "O importante não é ser o primeiro ou primeira, o importante é abrir caminhos.",
-          },
-          bio05: {
-            nome: "Carolina de Jesus",
-            imagem: "https://i.postimg.cc/Fdc5VKMJ/carolina-de-jesus.png",
-            descricao: "Carolina Maria de Jesus foi uma escritora brasileira, conhecida por seu livro Quarto de Despejo Diário de uma Favelada publicado em 1960. Considerada uma das primeiras e mais importantes escritoras negras do Brasil. A autora viveu boa parte de sua vida na favela do Canindé, na zona norte de São Paulo, sustentando a si mesma e seus três filhos como catadora de papéis. Em 1958 tem seu diário publicado sob o nome Quarto de Despejo, com auxílio do jornalista Audálio Dantas. O livro fez um enorme sucesso e chegou a ser traduzido para quatorze línguas.",
-            citacao: "Ah, comigo o mundo vai modificar-se. Não gosto do mundo como ele é.",
-          },
-          bio06: {
-            nome: "Rosa Parks",
-            imagem: "https://i.postimg.cc/K1q2Zw74/rosa-parks.png",
-            descricao: "Rosa Louise McCauley, mais conhecida por Rosa Parks, foi uma costureira negra norte-americana, símbolo do movimento dos direitos civis dos negros nos Estados Unidos. Ficou famosa, em 1º de dezembro de 1955, por ter-se recusado frontalmente a ceder o seu lugar no ônibus a um branco, tornando-se o estopim do movimento que foi denominado boicote aos ônibus de Montgomery e posteriormente viria a marcar o início da luta antissegregacionista.",
-            citacao: "⁠Você nunca deve ter medo do que está fazendo quando está certo.",
-         },
-        bio07: {
-          nome: "Liniker",
-          imagem: "Fotos/Liniker-1.jpg",
-          descricao:  "Liniker de Barros Ferreira Campos, mais conhecida como Liniker, é uma cantora e compositora brasileira, a cantora não teve o pai presente enquanto crescia e foi criada pela mãe, Ângela Barros. Liniker tem um irmão sete anos mais novo, Vítor, a cantora vem de uma linhagem de músicos e recebeu o nome em homenagem ao jogador de futebol inglês Gary Liniker. A cantora revelou em uma entrevista à revista Marie Claire que foi abusada sexualmente aos doze anos por conhecidos da família. Liniker, na mesma entrevista, disse que começou a se entender como mulher depois de conviver com a cantora Linn da Quebrada, elas estudaram Teatro juntas em Santo André em 2014. Liniker utiliza o pronome feminino (ela/dela) e, além de cantora, também é atriz, ela fez uma participação como ela mesma na série brasileira da Netflix 3% (2016 – 2020) e, em 2021, interpretou Cassandra em Manhãs de Setembro, série brasileira da Amazon Prime.",
-          citacao: "Eu não preciso ter uma certeza de ‘sou homem’ ou ‘sou mulher’, meu corpo é livre, meu corpo é um corpo político, ele merece a liberdade dele e eu preciso caminhar com isso, aceitar que eu sou assim.",
+        nome: "Carolina de Jesus",
+        imagem: "https://i.postimg.cc/Fdc5VKMJ/carolina-de-jesus.png",
+        descricao: "Carolina Maria de Jesus foi uma escritora brasileira, conhecida por seu livro Quarto de Despejo Diário de uma Favelada publicado em 1960. Considerada uma das primeiras e mais importantes escritoras negras do Brasil. A autora viveu boa parte de sua vida na favela do Canindé, na zona norte de São Paulo, sustentando a si mesma e seus três filhos como catadora de papéis. Em 1958 tem seu diário publicado sob o nome Quarto de Despejo, com auxílio do jornalista Audálio Dantas. O livro fez um enorme sucesso e chegou a ser traduzido para quatorze línguas.",
+        citacao: "Ah, comigo o mundo vai modificar-se. Não gosto do mundo como ele é.",
+        },     
+        bio03: {      
+        nome: "Conceição Evaristo",
+        imagem: "https://i.postimg.cc/Z9XzNNMS/conceicao-evaristo.png",
+        descricao:  "Maria da Conceição Evaristo de Brito nasceu em Belo Horizonte, em 1946. De origem humilde, migrou para o Rio de Janeiro na década de 1970. Graduada em Letras pela UFRJ, trabalhou como professora da rede pública de ensino da capital fluminense. É Mestre em Literatura Brasileira pela PUC do Rio de Janeiro, com a dissertação Literatura Negra: uma poética de nossa afro-brasilidade (1996), e Doutora em Literatura Comparada na Universidade Federal Fluminense, com a tese Poemas malungos, cânticos irmãos (2011), na qual estuda as obras poéticas dos afro-brasileiros Nei Lopes e Edimilson de Almeida Pereira em confronto com a do angolano Agostinho Neto.",
+        citacao: "O importante não é ser o primeiro ou primeira, o importante é abrir caminhos.",
         },
-        bio08: {
-          nome: "Elza Soares",
-          imagem: "Fotos/Elza.jpg",
-          descricao: "Elza Soares da Conceição (1930-2022) foi uma cantora e compositora brasileira. Um dos maiores nomes da MPB, Elza recebeu ano 2000, em Londres, o título de Melhor Cantora do Universo dado pela emissora BBC. Famosa pela voz rouca, Elza Soares foi um dos maiores nomes da música popular brasileira. Sua história de vida conta com tragédias e reviravoltas memoráveis.",
-          citacao: "Não tenho medo da morte, porque acho que não vou morrer. Vou virar purpurina!",
-        },
-        bio09: {
-          nome: "Maya Angelou",
-          imagem: "Fotos/Maya.jpg",
-          descricao: "Maya Angelou nasceu em St. Louis, Missouri, Estados Unidos, no dia 4 de abril de 1928. Nascida Marguerite Ann Johnson, Maya era o apelido de infância e Angelou foi o sobrenome de um de seus maridos. Maia exerceu diversas profissões – foi atriz e até condutora de bonde. Atou nos movimentos de Martin Luther King e Malcolm X. Nos anos 60 viveu no Egito e em Gana, trabalhando para os movimentos de direitos civis. Maya publicou dezenas de coletâneas de poemas. Sua consagração literária, no entanto, veio com o livro de memórias “I Know Why the Caged Bird Sing” (Eu Sei por que o Pássaro Canta na Gaiola), de 1969, no qual narra a infância no sul racista de seu país natal. No livro, relata a infância traumática após sofrer um estupro aos oito anos, pelo namorado de sua mãe. A violência a deixou muda durante anos. O livro se tornou o primeiro best-seller de não ficção escrito por uma afro-americana. Outros cinco livros de memórias vieram depois desse. Convidada com frequência para participar de eventos oficiais, sua posição como uma das personagens mais importantes da cultura americana foi consolidada quando recitou seu poema “On The Pulse of Morning” na posse do presidente Bill Clinton. Maya Angelou faleceu em Winston-Salem, na Carolina do Norte, Estados Unidos, no dia 28 de maio de 2014.",
-          citacao: "Toda vez que uma mulher se defende, sem nem perceber que isso é possível, sem qualquer pretensão, ela defende todas as mulheres.",
-       },
-        bio10: {
-        nome: "Michelle Obama",
-        imagem: "Fotos/michelle.jpg",
-        descricao:  "Bio Michelle",
-        citacao: "Citação Michelle",
-        },
-        bio11: {
+        bio04: {
         nome: "Dandara dos Palmares",
         imagem: "Fotos/dandara.jpg",
         descricao: "Bio Dandara",
         citacao: "Citação Dandara",
         },
-        bio12: {
+        bio05: {
+        nome: "Elza Soares",
+        imagem: "Fotos/Elza.jpg",
+        descricao: "Elza Soares da Conceição (1930-2022) foi uma cantora e compositora brasileira. Um dos maiores nomes da MPB, Elza recebeu ano 2000, em Londres, o título de Melhor Cantora do Universo dado pela emissora BBC. Famosa pela voz rouca, Elza Soares foi um dos maiores nomes da música popular brasileira. Sua história de vida conta com tragédias e reviravoltas memoráveis.",
+        citacao: "Não tenho medo da morte, porque acho que não vou morrer. Vou virar purpurina!",
+        },
+        bio06: {
+        nome: "Lélia Gonzalez",
+        imagem: "https://i.postimg.cc/2q1f3GTG/lelia-gonzalez.png",
+        descricao: "Lélia Gonzalez foi uma intelectual, política, professora e antropóloga brasileira. Ajudou a fundar instituições como o Movimento Negro Unificado (MNU), o Instituto de Pesquisas das Culturas Negras (IPCN), o Coletivo de Mulheres Negras N'Zinga e o Olodum. Sua militância em defesa da mulher negra levou-a ao Conselho Nacional dos Direitos da Mulher (CNDM), no qual atuou de 1985 a 1989. Foi candidata a deputada federal pelo PT, elegendo-se primeira suplente. Nas eleições seguintes, em 1986, candidatou-se a deputada estadual pelo PDT, novamente elegendo-se suplente.",
+        citacao:
+            "A gente não nasce negro, a gente se torna negro. É uma conquista dura, cruel e que se desenvolve pela vida da gente afora.",
+         },
+        bio07: {
+        nome: "Liniker",
+        imagem: "Fotos/Liniker-1.jpg",
+        descricao:  "Liniker de Barros Ferreira Campos, mais conhecida como Liniker, é uma cantora e compositora brasileira, a cantora não teve o pai presente enquanto crescia e foi criada pela mãe, Ângela Barros. Liniker tem um irmão sete anos mais novo, Vítor, a cantora vem de uma linhagem de músicos e recebeu o nome em homenagem ao jogador de futebol inglês Gary Liniker. A cantora revelou em uma entrevista à revista Marie Claire que foi abusada sexualmente aos doze anos por conhecidos da família. Liniker, na mesma entrevista, disse que começou a se entender como mulher depois de conviver com a cantora Linn da Quebrada, elas estudaram Teatro juntas em Santo André em 2014. Liniker utiliza o pronome feminino (ela/dela) e, além de cantora, também é atriz, ela fez uma participação como ela mesma na série brasileira da Netflix 3% (2016 – 2020) e, em 2021, interpretou Cassandra em Manhãs de Setembro, série brasileira da Amazon Prime.",
+        citacao: "Eu não preciso ter uma certeza de ‘sou homem’ ou ‘sou mulher’, meu corpo é livre, meu corpo é um corpo político, ele merece a liberdade dele e eu preciso caminhar com isso, aceitar que eu sou assim.",
+        },
+        bio08: {
+        nome:"Marielle Franco",
+        imagem: "https://i.postimg.cc/8fS8KSGX/marielle-franco.png",
+        descricao: "Marielle Francisco da Silva, conhecida como Marielle Franco, foi uma socióloga, feminista, militante dos direitos humanos e política brasileira. Filiada ao Partido Socialismo e Liberdade (PSOL), elegeu-se vereadora do Rio de Janeiro para a Legislatura 2017-2020, durante a eleição municipal de 2016, com a quinta maior votação. Crítica da intervenção federal no Rio de Janeiro e da Polícia Militar, denunciava constantemente abusos de autoridade por parte de policiais contra moradores de comunidades carentes. Em 14 de março de 2018, foi assassinada a tiros",
+        citacao: "Quantos mais vão precisar morrer para que essa guerra aos pobres acabe?",
+        },
+        bio09: {
+        nome: "Marsha P. Johnson",
+        imagem: "Fotos/marshapjohnson.jpg",
+        descricao: "Bio Marsha",
+        citacao: "Citação Marsha",
+       },
+        bio10: {
+        nome: "Maya Angelou",
+        imagem: "Fotos/Maya.jpg",
+        descricao: "Maya Angelou nasceu em St. Louis, Missouri, Estados Unidos, no dia 4 de abril de 1928. Nascida Marguerite Ann Johnson, Maya era o apelido de infância e Angelou foi o sobrenome de um de seus maridos. Maia exerceu diversas profissões – foi atriz e até condutora de bonde. Atou nos movimentos de Martin Luther King e Malcolm X. Nos anos 60 viveu no Egito e em Gana, trabalhando para os movimentos de direitos civis. Maya publicou dezenas de coletâneas de poemas. Sua consagração literária, no entanto, veio com o livro de memórias “I Know Why the Caged Bird Sing” (Eu Sei por que o Pássaro Canta na Gaiola), de 1969, no qual narra a infância no sul racista de seu país natal. No livro, relata a infância traumática após sofrer um estupro aos oito anos, pelo namorado de sua mãe. A violência a deixou muda durante anos. O livro se tornou o primeiro best-seller de não ficção escrito por uma afro-americana. Outros cinco livros de memórias vieram depois desse. Convidada com frequência para participar de eventos oficiais, sua posição como uma das personagens mais importantes da cultura americana foi consolidada quando recitou seu poema “On The Pulse of Morning” na posse do presidente Bill Clinton. Maya Angelou faleceu em Winston-Salem, na Carolina do Norte, Estados Unidos, no dia 28 de maio de 2014.",
+        citacao: "Toda vez que uma mulher se defende, sem nem perceber que isso é possível, sem qualquer pretensão, ela defende todas as mulheres.",
+        },
+        bio11: {
         nome: "Mercedes Baptista",
         imagem: "Fotos/mercedes.jpg",
         descricao: "Bio Mercedes",
         citacao: "Citação Mercedes",
+        },
+        bio12: {
+        nome: "Michelle Obama",
+        imagem: "Fotos/michelle.jpg",
+        descricao:  "Bio Michelle",
+        citacao: "Citação Michelle",
     },
         bio13: {
         nome: "Nina Simone",
@@ -86,10 +86,10 @@ function CarregaDicionario() {
         citacao: "Citação Oprah",
         },
         bio15: {
-        nome: "Marsha P. Johnson",
-        imagem: "Fotos/marshapjohnson.jpg",
-        descricao: "Bio Marsha",
-        citacao: "Citação Marsha",
+        nome: "Rosa Parks",
+        imagem: "https://i.postimg.cc/K1q2Zw74/rosa-parks.png",
+        descricao: "Rosa Louise McCauley, mais conhecida por Rosa Parks, foi uma costureira negra norte-americana, símbolo do movimento dos direitos civis dos negros nos Estados Unidos. Ficou famosa, em 1º de dezembro de 1955, por ter-se recusado frontalmente a ceder o seu lugar no ônibus a um branco, tornando-se o estopim do movimento que foi denominado boicote aos ônibus de Montgomery e posteriormente viria a marcar o início da luta antissegregacionista.",
+        citacao: "⁠Você nunca deve ter medo do que está fazendo quando está certo.",
     },
 };
 
